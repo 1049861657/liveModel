7e9a6d3f0b9f9380f8bccd54b2bcb2195112889f
yangjia <1049861657@qq.com>
2025-02-20 12:24:30 +0800

    测试1

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,37 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query']
-  })
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: ['query']
+//   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
+// import { PrismaClient } from "@prisma/client";
+// import { withOptimize } from "@prisma/extension-optimize";
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: process.env.NODE_ENV === 'development' ? ['query'] : []
+//   }).$extends(
+//     withOptimize(
+//       { apiKey: process.env.OPTIMIZE_API_KEY ?? "" ,
+//         enable: process.env.ENVIRONMENT === 'development'
+//       })
+//   );
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+
+import { PrismaClient } from "@prisma/client";
+import { withOptimize } from "@prisma/extension-optimize";
+
+export const prisma = new PrismaClient().$extends(
+  withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY!}),
+);
